32de9d7a68bd9a98cc26f4735aa7f70552b62520
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-21 13:14:13 +0000

    variables done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,13 @@
-console.log("akshat")
-console.log("goyal09")
+// console.log("akshat")
+// console.log("goyal09")
+
+const employeId = 123445
+var employeName = 'akshat'
+let employeWork = 'frontend'
+var employeee;
+
+// employeId = 3334455
+employeName = 'JiGoyal'
+employeWork = 'fullstack'
+
+console.table([employeName,employeWork,employeee])
