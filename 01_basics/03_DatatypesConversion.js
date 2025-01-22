74d4bc0e93901217e5f67591b5d9a59ee94bc77c
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-22 08:33:57 +0000

    Confusion

--- 01_basics/03_DatatypesConversion.js
+++ 01_basics/03_DatatypesConversion.js
@@ -1,6 +1,49 @@
 let Age = 24
 
 let newAge = String(Age)
-console.log(Age , typeof Age)
-console.log(newAge)
-console.log(typeof newAge)
+// console.log(Age , typeof Age)
+// console.log(newAge)
+// console.log(typeof newAge)
+
+let x = 3
+let y = x++ 
+let z = x++ 
+
+// console.log(x,y,z)
+
+let a = 3
+let b = ++a
+let c = ++a
+
+// console.log(a,b,c)
+
+let ak = 5
+let sh = ++ak
+let at = ++ak
+
+// console.log(ak,sh,at) //7 , 6 , 7 
+
+let go = 5
+let co = go++
+let oy = ++go //
+console.log(oy) // 7
+
+// console.log(oy)
+
+let ya = go++ 
+let al = ++go 
+console.log(al) //9
+
+let ay = oy++ //
+let qa = ++oy
+let ft = ay++ 
+let ra = qa++  
+
+// console.log(co,go,oy,ya,al) //9 , 8 , 
+console.log(ft , ra)
+
+
+
+
+
+
