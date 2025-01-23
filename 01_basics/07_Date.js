3c04a763e35fa0a065533c8fa689f1aeb866fd13
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-23 14:38:02 +0000

    Date and time

---
+++ 01_basics/07_Date.js
@@ -0,0 +1,13 @@
+let newDate = new Date()
+
+// console.log(newDate)
+// console.log(newDate.toLocaleString())
+// console.log(newDate.toDateString())
+
+let DateArr = new Date(2014,0,12)
+
+// console.log(DateArr.toLocaleString())
+// console.log(DateArr.toDateString())
+
+
+console.log(DateArr.getMonth()+1)
