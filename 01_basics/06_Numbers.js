d49ff93b7dc5ba94afb44edf862cfbd4a1adeeab
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-23 14:10:10 +0000

    Methods

---
+++ 01_basics/06_Numbers.js
@@ -0,0 +1,41 @@
+// let num = 40
+
+// console.log(num.toString())
+// console.log(num)
+
+// let number = 500000000
+
+// console.log(number.toLocaleString("en-IN"))
+
+// let nums = 123.251
+// console.log(nums.toPrecision(4))
+
+// const ballance = new Number(400)
+
+// console.log(ballance)
+
+// let min = 50 
+// let max = 75
+
+// let x = Math.floor(Math.random() * (max-min + 1 ) + min)
+// console.log(x)
+
+// let minAge = 18
+// let maxAge = 50
+
+// let x = Math.floor(Math.random () * (maxAge - minAge + 1) + minAge)
+// let y = maxAge - minAge + 1
+// console.log(x)
+
+// let num = 10;
+// let newNum = 20
+
+// console.log(Math.random()*( newNum - num + 1) + num )
+// let y = Math.random()
+// console.log(y)
+
+let nums = 15
+let maxNums = 20
+
+let x = Math.floor(Math.random()*(maxNums - nums + 1) + nums)
+console.log(x)
