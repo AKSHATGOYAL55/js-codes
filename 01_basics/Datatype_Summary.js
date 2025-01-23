4152f4c5928af934a95c62ba6b1e916d08368e0f
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-23 05:41:25 +0000

    Conversion

---
+++ 01_basics/Datatype_Summary.js
@@ -0,0 +1,34 @@
+let name = "akshat"
+let age = 24
+let isEmployee = false 
+let Salary = 566767676763467833n
+let score = null
+let profile;
+let age2 = Symbol(24)
+
+
+// console.log(typeof name)
+
+// console.log(null > 0) //comparision operator (false)
+// console.log(null === 0) //equality operator --- both operator work differently (false)
+// console.log(null >= 0) //only in comparison null convert value to 0 
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+
+
+
+// console.log(undefined == undefined);
+// console.log(undefined >= undefined);
+// console.log(undefined === undefined);
+
+// const myfunc = function() {
+//     console.log("Hello world")
+// }
+
+// console.log(typeof myfunc)
+// myfunc()
+
+console.log("2"== 2)
+console.log("2"=== 2)
