06b45384129c08b7d7db5ecc234c005f96c7ecaa
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-24 07:16:54 +0000

    Array

---
+++ 02_basics/02_Array.js
@@ -0,0 +1,25 @@
+let Team = ['India' , 'Pakistan' , 'SriLanka']
+let OtherTeam = ['NewZealand' , 'Australia' , 'England']
+
+let olympicTeam = ['china','indonesia','oman']
+
+// let newOne = console.log(Team.concat(OtherTeam))
+// console.log (typeof newOne)
+
+// let newTeam = console.log(...OtherTeam,...Team,...olympicTeam)
+// console.log(typeof newTeam)
+
+//---------Flate method means inside There is lots of array so use Flat()-------------
+
+// let Lots_of_Arr = [1,2,3,[4,5],6,7,[8,[9,10,[13,12,11]]]]
+// console.log(Lots_of_Arr.flat())
+// console.log(Lots_of_Arr.flat(2))
+// console.log(Lots_of_Arr.flat(Infinity))
+
+
+// console.log(Array.from("akshat"))  //from means seprated words => "a","k","s"..... create new Array
+// console.log(Array.of("akshat"))  //of means value will be convert in array.... Return new array
+
+
+// console.log(Array.from({"akshat":"goyal"}))
+
