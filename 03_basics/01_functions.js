06e5f8a4dceb9095a9a79fcffefe121ff5589dd6
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-25 07:50:06 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,45 @@
+function Akshat(num1 , num2){
+
+    console.log("Hello everyone")
+    // console.log(num1 + num2)
+}
+
+// Akshat(5,4)
+
+// -----------------------------------------------------------------
+
+function Akshat1(num1 , num2){
+
+    return num1 + num2
+} 
+
+// let newOne =  Akshat1(5,4)           //both type are same 
+// console.log(newOne)
+
+// console.log(Akshat1(4,7))            //but use this type 
+
+// ----------------------------------------------------------------------------------
+
+// function userActive(Akshat ){
+
+//     if(!Akshat){
+//         return `${Akshat} is not Active now`
+//     }
+//     return `${Akshat} is active `
+// }
+
+// console.log(userActive("Aayush"))
+
+// -----------------------------Example function ---------------------------------------------------
+
+function Dhoni_isplaying(fn ){
+    if(!fn){
+        return `Please call ${fn}`
+    }else{
+        return `yeah ${fn} is back in the field`
+    }
+}
+
+console.log(Dhoni_isplaying("Ms Dhoni"))
+
+// ----------------------------------------------------------------------------------
