e15923c1b71b0134bd0f1170955d11abe1ceebeb
AKSHATGOYAL55 <138345047+AKSHATGOYAL55@users.noreply.github.com>
2025-01-27 14:34:38 +0000

    while

---
+++ 05_iterations/02_do_while.js
@@ -0,0 +1,28 @@
+// let name = "harry"
+
+// while (name == "harry") {                          //infinite loop
+//     console.log("right name")
+// };
+
+// ----------------------------------------------------------
+
+
+// let index = 0
+
+// while (index <= 10) {
+//     console.log(`value of array is ${index}`)
+//     index = index + 1
+// }
+
+// ----------------------------------------------------------------
+
+let arr = ["spiderman","Superman","Hulk","Krish"]
+
+let newArr = 0
+
+while (newArr < arr.length) {
+    console.log(`superhero name is ${arr[newArr]}`)
+    newArr = newArr + 1
+}
+
+// -------------------------------------------------------------------
